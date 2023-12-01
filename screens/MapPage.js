503a70ca91abf839505f9541f6ab5ac2e1a4937d
Henrik Bang-Olsen <henrikbang01@gmail.com>
2023-12-01 13:33:33 +0100

    Added cities to markers on map

--- screens/MapPage.js
+++ screens/MapPage.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { getDistance, isPointWithinRadius } from 'geolib';
-import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
 import Slider from '@react-native-community/slider';
+
+import citiesData from '../it.json';
 
 const milanRegion = {
   latitude: 45.464664,
@@ -12,48 +13,82 @@ const milanRegion = {
   longitudeDelta: 0.2221,
 };
 
-const testDistance = getDistance(
-  { latitude: 51.5103, longitude: 7.49347 },
-  { latitude: 51.5103, longitude: 7.39347 }
-);
-
-const testRadius = isPointWithinRadius(
-  { latitude: milanRegion.latitude, longitude: milanRegion.longitude },
-  { latitude: milanRegion.latitude + 0.02, longitude: milanRegion.longitude + 0.02 },
-  3000
-);
-console.log(testRadius)
-
 export default function App() {
-
-  const [visible, setVisible] = useState(false);
-  const hideMenu = () => setVisible(false);
-  const showMenu = () => setVisible(true);
-
-  const [range, setRange] = useState("0 km");
-
+  const [range, setRange] = useState(0);
+  const [markers, setMarkers] = useState([]);
   const [isViewVisible, setIsViewVisible] = useState(false);
+  const [mainLat, setMainLat] = useState(45.464664);
+  const [mainLng, setMainLng] = useState(9.179540);
+
+  const updateMarkers = (radius) => {
+
+    const center = { latitude: mainLat, longitude: mainLng };
+
+
+    const filteredCities = citiesData.filter((city) => {
+      const cityLat = parseFloat(city.lat);
+      const cityLng = parseFloat(city.lng);
+      return isPointWithinRadius(center, { latitude: cityLat, longitude: cityLng }, radius);
+    });
+
+    const newMarkers = filteredCities.map((city) => ({
+      key: city.city,
+      coordinate: {
+        latitude: parseFloat(city.lat),
+        longitude: parseFloat(city.lng),
+      },
+      title: city.city,
+      description: `Population: ${city.population}`,
+    }));
+
+    setMarkers(newMarkers);
+  };
+
+  const handleSliderChange = (value) => {
+    setRange(parseInt(value));
+  };
+
+  const handleConfirmButtonPress = () => {
+    updateMarkers(range * 1000);
+    setIsViewVisible(false);
+  };
 
   const toggleViewVisibility = () => {
     setIsViewVisible(!isViewVisible);
   };
+
+  const handleMapPress = (event) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    setMainLat(latitude);
+    setMainLng(longitude);
+  }
+
+
 
   return (
     <View style={styles.container}>
       <View style={styles.mapbox}>
         {isViewVisible && (<View style={styles.card}>
           <Text style={{ fontSize: 20 }}>Choose your distance</Text>
-          <Slider onValueChange={value => setRange(parseInt(value) + " km")} style={{ width: 200, height: 60 }} minimumValue={0} maximumValue={500} minimumTrackTintColor="white" maximumTrackTintColor="white" />
-          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{range}</Text>
-          <Text style={styles.confirmButton} onPress={toggleViewVisibility}>Confirm distance</Text>
+          <Slider onValueChange={handleSliderChange} style={{ width: '90%', height: 60 }} minimumValue={0} maximumValue={1250} step={20} value={range} minimumTrackTintColor="white" maximumTrackTintColor="white" />
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{range + " km"}</Text>
+          <Text style={styles.confirmButton} onPress={handleConfirmButtonPress}>Confirm distance</Text>
         </View>)}
         <Text onPress={toggleViewVisibility} style={styles.settingsButton}>Show menu</Text>
+
         <MapView style={styles.map} initialRegion={milanRegion}>
-          <Marker id="marker1" coordinate={{ latitude: milanRegion.latitude, longitude: milanRegion.longitude }} />
-          <Marker id="marker2" coordinate={{ latitude: milanRegion.latitude + 0.02, longitude: milanRegion.longitude + 0.02 }} />
+          <Marker coordinate={{ latitude: 45.464664, longitude: 9.179540 }} pinColor='blue' draggable onDragEnd={handleMapPress}></Marker>
+          {markers.map((marker) => (
+            <Marker
+              key={marker.key}
+              coordinate={marker.coordinate}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
         </MapView>
       </View>
-    </View >
+    </View>
   );
 }
 
@@ -70,13 +105,10 @@ const styles = StyleSheet.create({
     zIndex: -10,
   },
   mapbox: {
-    width: '95%',
-    height: '92%', // Consider adjusting this height to leave space for the settings button
+    width: '100%',
+    height: '100%',
     position: 'relative',
     zIndex: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: 'black',
     overflow: 'hidden',
   },
   sliderbox: {
@@ -94,13 +126,14 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightblue',
     width: 60, // Adjust the width as needed
     padding: 10,
-    top: 435, // Adjust the top position as needed
+    bottom: 10, // Adjust the top position as needed
     right: 10, // Adjust the right position as needed
     alignItems: 'center',
     textAlign: "center",
     borderRadius: 10,
     borderWidth: 2,
-    borderColor: "black"
+    borderColor: "black",
+    overflow: "hidden"
 
   },
   card: {
@@ -115,7 +148,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: "center",
     padding: 10,
-    gap: 15
+    gap: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+
+    elevation: 12,
   },
   confirmButton: {
     fontSize: 20,
@@ -128,9 +170,4 @@ const styles = StyleSheet.create({
     padding: 3,
     overflow: "hidden"
   }
-
-
 });
-
-
-
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,32 +1,64 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import Box from "proximity/components/Box.js";
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 
+import citiesData from '../it.json';
+import { isPointWithinRadius } from 'geolib';
 
-export default function App() {
+const CityList = () => {
+    const renderCityItem = (city) => {
+        // Call your function for each city here
+        console.log(`Processing city: ${city.city}`);
+        const cityLng = parseFloat(city.lng);
+        const cityLat = parseFloat(city.lat);
+        const isWithin = isPointWithinRadius({ latitude: 45.464664, longitude: 9.179540 }, { latitude: cityLat, longitude: cityLng }, 100000)
+        if (isWithin) {
+            console.log("City is within radius")
+        }
+
+        return (
+            <View style={styles.cityItem}>
+                <Text style={styles.cityName}>{city.city}</Text>
+                <Text>Population: {city.population}</Text>
+                <Text>Latitude: {city.lat}</Text>
+                <Text>Longitude: {city.lng}</Text>
+                <Text>Admin Name: {city.admin_name}</Text>
+            </View>
+        );
+    };
+
+    const renderItem = ({ item }) => {
+        return renderCityItem(item);
+    };
 
     return (
-
         <View style={styles.container}>
-            <Text style={styles.text}> Dette er profilsiden </Text>
-            <Box></Box>
+            <Text style={styles.header}>List of Cities</Text>
+            <FlatList
+                data={citiesData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.city}
+                onEndReachedThreshold={0.1} // Adjust as needed
+            />
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: "lightblue",
-        alignItems: "center",
-        justifyContent: "center"
+        padding: 20,
     },
-    text: {
-        fontSize: 30,
-        fontWeight: "bold",
-        color: "white"
-    }
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    cityItem: {
+        marginBottom: 15,
+    },
+    cityName: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
 });
 
-
-
+export default CityList;
